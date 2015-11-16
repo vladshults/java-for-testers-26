f79c95af43146570c1b0d4aee861b921a753cd2a
vladshults <vladshulkevich@gmail.com>
2015-11-16 13:01:48 +0300

    16_11_2015_HW#13_Shulkevich

---
+++ start-test.bat
@@ -0,0 +1,7 @@
+cd G:\GitHub\java-for-testers-26\
+java -cp bin;^
+G:\SSS\selenium-server-standalone-2.48.2.jar;^
+G:\SSS\xstream-1.4.8.jar;^
+G:\SSS\xmlpull-1.1.3.1.jar;^
+G:\SSS\xpp3_min-1.1.4c.jar; ^
+org.testng.TestNG testng-customsuite.xml
